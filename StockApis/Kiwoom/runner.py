6e7610f6815982fada028bce160c39307613e4a5
gimo <goodmoskito@gmail.com>
2023-07-15 15:54:46 +0900

    runner 수정

--- StockApis/Kiwoom/runner.py
+++ StockApis/Kiwoom/runner.py
@@ -1,24 +1,58 @@
 import threading
+from StockApis.Kiwoom import kiwoom, receiver
 
-from StockApis.Kiwoom import kiwoom, receiver, consts
 
+class Sender:
+    """
+        Sender는 TxReceiver와 RealTxThread의 마스터임.
 
-if __name__ == '__main__':
-    ctrl = kiwoom.Controller()
-    ctrl_lock = threading.Lock()
-    q_ctrl = receiver.QueueController()
-    real_queue_dict = {
-        consts.RealReg.CurrentPrice: receiver.QueueController(),
-        consts.RealReg.Orderbook: receiver.QueueController()
-    }
+        kiwoom controller를 연결하고
+        lock과 queue를 지정하며
+        관련 스레드를 실행하고 real reg에 코드를 등록하는 역할을 함.
+    """
+    def __init__(self, code_list):
+        self.code_list = code_list
 
-    tx_thread = receiver.TxEventReceiver(ctrl, ctrl_lock, q_ctrl)
-    real_tx_thread = receiver.RealTxEventReceiver(ctrl, ctrl_lock, real_queue_dict)
-    tx_thread.run()
+        self.controller = kiwoom.Controller()
+        self.controller_lock = threading.Lock()
 
-    common_object = kiwoom.Common(ctrl, q_ctrl)
+        self.queue_controller = receiver.QueueController()
 
-    stock_code_dict = common_object.get_stock_codes()
+        self.tx_thread = receiver.TxEventReceiver(
+            self.controller,
+            self.controller_lock,
+            self.queue_controller
+        )
+        self.real_tx_thread = receiver.RealTxEventReceiver()
 
-    price_object = kiwoom.Price(ctrl, q_ctrl, stock_code_dict["kosdaq"][-1])
-    price_object.get_current_price()
+        self.tx_thread.run()
+        self.real_tx_thread.run()
+
+        self.real_current_price_setter()
+        self.real_orderbook_setter()
+
+    def run(self):
+        while True:
+            pass
+
+    def real_current_price_setter(self):
+        self.real_current_price_block = receiver.RealRegBlock(
+            self.controller,
+            self.controller_lock,
+        )
+
+        self.real_current_price_block.define_to_current_price_block()
+        self.real_current_price_block.init_block_list(
+            self.code_list
+        )
+
+    def real_orderbook_setter(self):
+        self.real_orderbook_block = receiver.RealRegBlock(
+            self.controller,
+            self.controller_lock,
+        )
+
+        self.real_orderbook_block.define_to_current_price_block()
+        self.real_orderbook_block.init_block_list(
+            self.code_list
+        )
