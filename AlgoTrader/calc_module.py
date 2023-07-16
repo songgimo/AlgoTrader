21d75071c72152e8f1608a3c6cf315bfc8f92592
gimo <goodmoskito@gmail.com>
2023-07-16 15:06:31 +0900

    calculate logic updated.

--- AlgoTrader/calc_module.py
+++ AlgoTrader/calc_module.py
@@ -1,4 +1,4 @@
-import re
+import operator
 import threading
 import consts
 import indicators
@@ -8,7 +8,15 @@ from StockApis.Kiwoom import consts
 
 
 class CandleContainer:
-    def __init__(self):
+    """
+        Candle의 OHLC값이 들어간다.
+        Refresh Thread를 통해 지속적으로 Write된다.
+        Indicator들로부터 Read된다.
+    """
+    def __init__(self, symbol: str):
+        # symbol은 005790같은 주식 코드 값이나, eth같은 symbol값이 입력된다.
+        self.symbol = symbol
+
         self.candle = []
         self.close = []
         self.high = []
@@ -16,63 +24,85 @@ class CandleContainer:
 
 
 class IndicatorNode:
+    """
+        최초 Stock Thread로부터 불러와지고 핸들링된다.
+        유저로부터 입력된 보조지표 string값과 parameter를 입력받고 가공한다.
+        이후 지표 Object를 가져오고 Stock Thread로 리턴되며, 조작된다.
+    """
     def __init__(
             self,
-            candle_container,
-            candle_lock,
-            val
+            candle_container: CandleContainer,
+            candle_lock: threading.Lock,
+            val: str
     ):
         self._candle_container = candle_container
         self._candle_lock = candle_lock
-        self.val = self.convert_string(val)
 
-    def convert_string(self, val):
-        data = re.findall("[\d\w]+", val.lower())
+        self.indicator_class = self.convert_string(val)
 
-        name, indicate_data = data[0], data[1:]
-        if consts.IndicatorName.GC in name:
-            return indicators.GoldenCross(indicate_data, self._candle_container, self._candle_lock)
+    def convert_string(self, val: str):
+        class_dict = {
+            consts.IndicatorName.GC: indicators.GoldenCross,
+            consts.IndicatorName.BB: indicators.BollingerBand,
+            consts.IndicatorName.RSI: indicators.RSI,
+            consts.IndicatorName.MACD: indicators.MACD,
+            consts.IndicatorName.STC: indicators.Stochastic,
+            consts.IndicatorName.CCI: indicators.CCI
+        }
+        setting_object = indicators.Settings(val)
+        name = setting_object.algo_name
 
-        elif consts.IndicatorName.BB in name:
-            return indicators.BollingerBand(indicate_data, self._candle_container, self._candle_lock)
+        indicator_class = class_dict.get(name, None)
 
-        elif consts.IndicatorName.RSI in name:
-            return indicators.RSI(indicate_data, self._candle_container, self._candle_lock)
+        if indicator_class:
+            return indicator_class(setting_object, self._candle_container, self._candle_lock)
 
-        elif consts.IndicatorName.MACD in name:
-            return indicators.MACD(indicate_data, self._candle_container, self._candle_lock)
-
-        elif consts.IndicatorName.STC in name:
-            return indicators.Stochastic(indicate_data, self._candle_container, self._candle_lock)
-
-        elif consts.IndicatorName.CCI in name:
-            return indicators.CCI(indicate_data, self._candle_container, self._candle_lock)
+        return None
 
 
 class Stock(threading.Thread):
-    def __init__(self, candle_container, stock_code, left, right):
+    """
+        main process로부터 핸들링되는 Thread이며, 트레이딩할 거래 지표를 정의한다.
+        이후 지속적으로 N개의 인디케이터를 감시하고 트리거가 발동하는 경우 Kiwoom으로 거래 트리거를 보낸다.
+    """
+    def __init__(self, container, lock, left, right, op):
         super(Stock, self).__init__()
-        self._stock_code = stock_code
-
-        self._candle_container = candle_container
-        self._candle_lock = threading.Lock()
+        self._candle_container = container
+        self._candle_lock = lock
 
         self._left_indicator = IndicatorNode(self._candle_container, self._candle_lock, left)
         self._right_indicator = IndicatorNode(self._candle_container, self._candle_lock, right)
 
+        self._op = getattr(operator, f"{op}_")
+
     def run(self) -> None:
         while True:
-            if self._left_indicator.val.calculator() and self._right_indicator.val.calculator():
-                pass
+            left_indicator = self._left_indicator.indicator_class
+            right_indicator = self._right_indicator.indicator_class
+
+            left_indicator.calculator()
+            right_indicator.calculator()
+
+            if self._op(left_indicator.check_values(), right_indicator.check_values()):
+                data = {"symbol": self._candle_container.symbol}
+                REDIS_SERVER.set(consts.RequestHeader.Trade, data)
 
 
-class Refresh(threading.Thread):
-    def __init__(self, candle_container):
+class StockRefresher(threading.Thread):
+    def __init__(self, thread_dict):
         super().__init__()
-        self._candle_container = candle_container
+        self._thread_dict = thread_dict
 
     def run(self) -> None:
         while True:
-            data = REDIS_SERVER.get(consts.RealReg.CurrentPrice)
-            self._candle_container.close = data
+            current_price_dict = REDIS_SERVER.get(consts.RealReg.CurrentPrice)
+
+            for stock_code in current_price_dict:
+                if stock_code not in self._thread_dict.keys():
+                    continue
+
+                container = self._thread_dict[stock_code]["container"]
+                price_data = current_price_dict[stock_code]
+
+                container.close = price_data
 
