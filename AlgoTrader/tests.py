a96a8709ab52238366041e3534c687ccc1062a5e
gimo <goodmoskito@gmail.com>
2023-07-23 15:00:20 +0900

    calculator candle test logic

--- AlgoTrader/tests.py
+++ AlgoTrader/tests.py
@@ -19,8 +19,12 @@ class TestClass:
         """
             candle spec
                 GC: [candle_size, short_period, long_period, method]
+                BB: [candle_size, period, deviation, line, method]
+                RSI: [candle_size, period, bound, method]
+
                 Stochastic: [candle_size, period, period_m, period_t, reference, method]
                     - reference: fastK, fastD, slowK, slowD
+                CCI: [candle_size, period, bound, method]
 
         """
         self.candle_container = candle_container
@@ -30,23 +34,120 @@ class TestClass:
         self.rt = "GoldenCross[1, 14, 50, bound_upper]"
         self.op = "and"
 
-    def indicator_node_checker(self):
-        left = IndicatorNode(
+    def test_golden_cross(self):
+        """
+            golden cross
+                - candle_size: 캔들의 규모 (실시간, 1분, 5분, 10분, 30분 등등)
+                - short period: 단기 이동 평균 기간
+                - long period: 장기 이동 평균 기간
+                - method: 트리거 발생 조건
+
+        """
+        string_ = "GoldenCross[1, 14, 50, bound_upper]"
+
+        indi_node = IndicatorNode(
             self.candle_container,
             self.lock,
-            self.lt
+            string_
         )
 
-        right = IndicatorNode(
+        self.test_by_indicator_class(indi_node.indicator_class)
+
+    def test_bollinger_band(self):
+        """
+            bollinger band
+                - candle_size: 캔들의 규모 (실시간, 1분, 5분, 10분, 30분 등등)
+                - period: 계산될 캔들의 갯수
+                - deviation: 표준 편차 증폭 계수
+                - line: lower, middle, upper의 트리거 라인 설정
+                - method: 트리거 발생 조건
+        """
+        string_ = "BollingerBand[1, 14, 2, upper, bound_upper]"
+
+        indi_node = IndicatorNode(
             self.candle_container,
             self.lock,
-            self.rt
+            string_
         )
 
-        right_result = self.test_by_indicator_class(right.indicator_class)
-        left_result = self.test_by_indicator_class(left.indicator_class)
+        self.test_by_indicator_class(indi_node.indicator_class)
 
-        return left_result, right_result
+    def test_rsi(self):
+        """
+            rsi
+                - candle_size: 캔들의 규모 (실시간, 1분, 5분, 10분, 30분 등등)
+                - period: 계산될 캔들의 갯수
+                - bound: 숫자 형태의 트리거 라인 설정
+                - method: 트리거 발생 조건
+        """
+        string_ = "RSI[1, 14, 30, bound_upper]"
+
+        indi_node = IndicatorNode(
+            self.candle_container,
+            self.lock,
+            string_
+        )
+
+        self.test_by_indicator_class(indi_node.indicator_class)
+
+    def test_macd(self):
+        """
+            macd
+                - candle_size: 캔들의 규모 (실시간, 1분, 5분, 10분, 30분 등등)
+                - short_period: 단기 지수 이동 평균 기간
+                - long_period: 장기 지수 이동 평균 기간
+                - signal_period: MACD 시그널 지수 이동 평균 기간
+                - bound: 숫자 형태의 트리거 라인 설정
+                - reference: Histogram/Line의 참조할 지표 상세
+                - method: 트리거 발생 조건
+        """
+        string_ = "MACD[1, 13, 26, 9, 100, Histogram, bound_upper]"
+
+        indi_node = IndicatorNode(
+            self.candle_container,
+            self.lock,
+            string_
+        )
+
+        self.test_by_indicator_class(indi_node.indicator_class)
+
+    def test_stochastic(self):
+        """
+            stochastic
+                - candle_size: 캔들의 규모 (실시간, 1분, 5분, 10분, 30분 등등)
+                - period: 계산될 캔들의 갯수
+                - period_m: fastD를 계산할 기간
+                - period_t: slowD를 계산할 기간
+                - reference: fastK, fastD, slowK, slowD의 참조할 지표 상세
+                - method: 트리거 발생 조건
+
+        """
+        string_ = "Stochastic[1, 14, 50, fastK, bound_lower]"
+        indi_node = IndicatorNode(
+            self.candle_container,
+            self.lock,
+            string_
+        )
+
+        self.test_by_indicator_class(indi_node.indicator_class)
+
+    def test_cci(self):
+        """
+            cci
+                - candle_size: 캔들의 규모 (실시간, 1분, 5분, 10분, 30분 등등)
+                - period: 계산될 캔들의 갯수
+                - bound: 숫자 형태의 트리거 라인 설정
+                - method: 트리거 발생 조건
+
+        """
+        string_ = "CCI[1, 14, 50, bound_upper]"
+        indi_node = IndicatorNode(
+            self.candle_container,
+            self.lock,
+            string_
+        )
+
+        self.test_by_indicator_class(indi_node.indicator_class)
 
     def test_by_indicator_class(self, indi_class):
         indi_class.calculator()
@@ -66,4 +167,3 @@ if __name__ == '__main__':
     cd.set_candle(test_orderbook["005930"])
     cd.set_close(test_orderbook["005930"])
     tc = TestClass(cd)
-    l, r = tc.indicator_node_checker()
